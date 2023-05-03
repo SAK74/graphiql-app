66293cb333967a6aa29944c11df72209885ea4f0
‘Anastasiia <miss.iuferova@yandex.ru>
2023-05-03 16:57:53 +0200

    feat: add shadow to header

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
 export default function MainPage() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+      <div className="w-100 bg-blue">New</div>
+    </div>
+  );
 }
