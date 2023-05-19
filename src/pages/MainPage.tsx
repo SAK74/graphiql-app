eee56b90147a2b5d8b12f4e05ab6495692feac37
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2023-05-19 13:41:01 +0200

    fixed warnings

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -69,7 +69,7 @@ export default function MainPage() {
             <div>
               <div className="rounded-t-lg shadow-md p-4">
                 <div className="flex justify-between">
-                  <p className="text-lg font-semibold p-2">{t('mainPage.request')}</p>
+                  <p className="text-lg font-semibold">{t('mainPage.request')}</p>
                   <button
                     onClick={runRequest}
                     className="py-2 px-3 hover:bg-light-blue hover:text-black text-white h-10 bg-dark-blue rounded-md"
