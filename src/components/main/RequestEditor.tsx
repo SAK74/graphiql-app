e93b4f592e844d22d9bc37f164f61de1ac7085d9
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2023-05-25 18:32:07 +0200

    implemented query syntax errors handler

--- src/components/main/RequestEditor.tsx
+++ src/components/main/RequestEditor.tsx
@@ -3,16 +3,22 @@ import { useQueryContext } from './QueryProvider';
 import RequestArea from './RequestArea';
 
 export const RequestEditor = () => {
-  const { runRequest } = useQueryContext();
-  const {} = useTranslation();
+  const { runRequest, isSyntaxError } = useQueryContext();
   const { t } = useTranslation();
+
+  const handleRun = () => {
+    if (!isSyntaxError) {
+      return runRequest();
+    }
+  };
   return (
     <div className="rounded-t-lg shadow-md p-4">
       <div className="flex justify-between">
         <p className="text-lg font-semibold p-2">{t('mainPage.request')}</p>
         <button
-          onClick={runRequest}
-          className="py-2 px-3 hover:bg-light-blue hover:text-black text-white h-10 bg-dark-blue rounded-md"
+          disabled={isSyntaxError}
+          onClick={handleRun}
+          className="py-2 px-3 disabled:bg-gray-600 hover:bg-light-blue hover:text-black text-white h-10 bg-dark-blue rounded-md"
         >
           {t('mainPage.button')}
         </button>
