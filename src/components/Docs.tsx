b29fcfd0e564a3e471a4064e63cfba19ac0d9b96
‘Anastasiia <miss.iuferova@yandex.ru>
2023-05-26 18:53:15 +0200

    fix: move tree component

--- src/components/Docs.tsx
+++ src/components/Docs.tsx
@@ -1,11 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import Spinner from './Spinner';
 import { Suspense, lazy } from 'react';
+const TreeComponent = lazy(() => import('./main/Docs/Tree'));
 
 const Docs = () => {
   const { t } = useTranslation();
-
-  const TreeComponent = lazy(() => import('./main/Docs/Tree'));
 
   return (
     <div className="p-4">
