a169da2d14abe1630e12531d2c17a8e66ffd2fcb
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2023-05-26 13:09:21 +0200

    added localization for about project description

--- src/pages/WelcomePage/AboutProject.tsx
+++ src/pages/WelcomePage/AboutProject.tsx
@@ -10,39 +10,23 @@ export default function AboutProject() {
         {t('welcomePage.about.title')}
       </h2>
       <div className="text-xl leading-8 flex gap-6 flex-col">
+        <p>{t('welcomePage.about.aboutApp')}</p>
         <p>
-          This is a GraphQl playground web app that provides a GraphQL interface available to
-          authorized users after registration. Users land on a welcome page where they can sign in
-          or sign up if they are not authorized. Authorized users are directed to the main page,
-          which includes a GraphQL interface.
-        </p>
-        <p>
-          The main page interface allows users to send GraphQL queries to&nbsp;
+          {t('welcomePage.about.queries')}
           <a
             className="text-dark-blue"
             href="https://studio.apollographql.com/public/rick-and-morty-a3b90u/variant/current/home"
           >
-            Rick and Morty API
+            {t('welcomePage.about.apiName')}
           </a>
-          . It includes a request editor for composing queries, a variables editor for managing
-          query variables, and a lazy-loaded documentation explorer, providing insights into the
-          API&apos;s schema when a successful schema response is received.
+          {t('welcomePage.about.aboutApi')}
         </p>
-        <p>
-          The app has a sticky header that changes color when scrolling and includes a sign-out
-          button. It also supports localization in Russian and English. Authentication and
-          authorization are handled using Firebase with email options. Client-side validation
-          ensures secure input. The footer contains links to the authors&apos; GitHub profiles and
-          the course reference.
-        </p>
-        <p>
-          Overall, this project showcases a robust implementation of a GraphQL web app with key
-          features for an enhanced user experience.
-        </p>
+        <p>{t('welcomePage.about.aboutStyles')}</p>
+        <p>{t('welcomePage.about.overall')}</p>
       </div>
       <div>
         <h2 className="text-3xl mt-16 md:text-4xl pb-8 font-mono text-dark-blue">
-          Technologies used
+          {t('welcomePage.technologies.title')}
         </h2>
         <div className="flex gap-10 flex-col md:flex-row items-center justify-center">
           {techs.map((item, index) => {
