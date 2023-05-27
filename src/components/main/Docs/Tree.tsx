70190ede0620bea0cff3d551b725dcc1c8078d3f
‘Anastasiia <miss.iuferova@yandex.ru>
2023-05-27 14:01:35 +0200

    feat: add additional spinner

--- src/components/main/Docs/Tree.tsx
+++ src/components/main/Docs/Tree.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useTreeState, Tree } from 'react-hyper-tree';
 import { TreeNode } from 'types/types';
 import useDataRender from './useDataRender';
+import Spinner from 'components/Spinner';
 
 const TreeComponent = () => {
   const [data, setData] = useState<TreeNode>({
@@ -24,6 +25,7 @@ const TreeComponent = () => {
   });
 
   if (error) return <p>Error: {error.message}</p>;
+  if (data.id === '0') return <Spinner />;
 
   return schemaData && <Tree {...required} {...handlers} />;
 };
